47e6a387287b20389a54c5802e7c02f11fa1f735
Gabrielkaos <gabrielmontes155@gmail.com>
2025-12-01 14:06:16 +0800

    logger finished

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,5 +4,10 @@ require('dotenv').config()
 module.exports = {
     server:{
         port:process.env.PORT
+        env:process.env.ENV
+    },
+
+    logger:{
+        log_level:process.env.PORT
     }
 }
---
+++ backend/utils/logger.js
@@ -0,0 +1,35 @@
+const winston = require("winston")
+const path = require("path")
+const config = require("../config/config")
+
+
+const logsDir = path.join(__dirname,'../logs')
+
+const logFormat = winston.format.combine(
+    winston.format.timestamp({format:'YYYY-MM-DD HH:mm:ss'}),
+    winston.format.errors({stack:true}),
+    winston.format.splat(),
+    winston.format.json()
+)
+
+const logger = winston.createLogger({
+    level: config.logger.log_level,
+    format:logFormat,
+    defaultMeta:{service:'inventory-api'},
+    transports : [
+        new winston.transports.File({
+            filename:path.join(logsDir,'error.log'),
+            level:'error',
+            maxsize:5242880,
+            maxFiles:5,
+        }),
+
+        new winston.transports.File({
+            filename:path.join(logsDir,'combined.log'),
+            maxsize:5242880,
+            maxFiles:5,
+        }),
+    ]
+})
+
+if (config.)
